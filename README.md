f0c4386a3cbb58821ed7853ffc57a9f078be38d7
jgauthi <jgauthi@users.noreply.github.com>
2018-12-31 22:47:01 +0100

    Component: create App.js (hello world)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install
+`npm install`
+
+# Start server
+`npm start`
+
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import createHistory from 'history/createBrowserHistory';
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "react-router-redux";
+import App from "./components/App";
+import {Route} from "react-router";
+
+const store = createStore(
+    state => state
+);
+const history = createHistory();
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Route path="/" component={App}/>
+        </ConnectedRouter>
+    </Provider>
+), document.getElementById('root'));
