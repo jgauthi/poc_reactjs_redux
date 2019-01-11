d0266067d8077b0fd0c455b33baf8203955f0791
jgauthi <jgauthi@users.noreply.github.com>
2019-01-11 18:04:06 +0100

    Use Post from Symfony API - Use only response body, apply thunk middleware

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,8 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT = 'http://localhost/mindsymfony4/public/api';
+const responseBody = response => response.body;
 
 export const requests = {
-  get: (url) => superagent.get(`${API_ROOT}/${url}`)
+    get: (url) => superagent.get(`${API_ROOT}${url}`).then(responseBody)
 };
--- src/reducers/blogPostList.js
+++ src/reducers/blogPostList.js
@@ -1,20 +1,45 @@
-import {BLOG_POST_LIST, BLOG_POST_LIST_ADD} from "../actions/actions";
+import {
+    BLOG_POST_LIST_ADD,
+    BLOG_POST_LIST_ERROR,
+    BLOG_POST_LIST_RECEIVED,
+    BLOG_POST_LIST_REQUEST
+} from "../actions/actions";
 
 export default(state = {
     posts: null,
-    anotherState: 'Hi'
+    isFetching: false
 }, action) => {
     switch (action.type) {
-        case BLOG_POST_LIST:
-            return {
+        case BLOG_POST_LIST_REQUEST:
+            state = {
                 ...state,
-                posts: action.data
+                isFetching: true
             };
+            console.log(state);
+            return state;
+        case BLOG_POST_LIST_RECEIVED:
+            state = {
+                ...state,
+                posts: action.data['hydra:member'],
+                isFetching: false
+            };
+            console.log(state);
+            return state;
+        case BLOG_POST_LIST_ERROR:
+            state = {
+                ...state,
+                isFetching: false,
+                posts: null
+            };
+            console.log(state);
+            return state;
         case BLOG_POST_LIST_ADD:
-            return {
+            state = {
                 ...state,
                 posts: state.posts ? state.posts.concat(action.data) : state.posts
             };
+            console.log(state);
+            return state;
         default:
             return state;
     }
