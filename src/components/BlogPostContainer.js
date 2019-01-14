98edcd9bdce86267cffcbcf03c351504759b5125
jgauthi <jgauthi@users.noreply.github.com>
2019-01-14 12:59:36 +0100

    Route: blog Post url (new component BlogPost + Container)

---
+++ src/components/BlogPostContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class BlogPostContainer extends React.Component {
+    componentDidMount() {
+        console.log(this.props);
+        console.log(this.props.match.params.id);
+    }
+
+    render() {
+        return (<div>Hello from BlogPost!</div>);
+    }
+}
