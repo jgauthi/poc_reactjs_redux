815cc98ddfc3ab67eb31c42a0e533a1585a73c15
jgauthi <jgauthi@users.noreply.github.com>
2019-01-14 13:31:31 +0100

    blogPost: fetching item from API (new reducer, constant action)

--- src/components/BlogPostContainer.js
+++ src/components/BlogPostContainer.js
@@ -1,12 +1,25 @@
 import React from "react";
+import {blogPostFetch} from "../actions/actions";
+import {connect} from "react-redux";
 
-export default class BlogPostContainer extends React.Component {
+const mapStateToProps = state => ({
+    ...state.blogPost
+});
+
+const mapDispatchToProps = {
+    blogPostFetch
+};
+
+class BlogPostContainer extends React.Component {
     componentDidMount() {
         console.log(this.props);
         console.log(this.props.match.params.id);
+        this.props.blogPostFetch(this.props.match.params.id).then(_ => console.log(this.props.post));
     }
 
     render() {
         return (<div>Hello from BlogPost!</div>);
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogPostContainer);
