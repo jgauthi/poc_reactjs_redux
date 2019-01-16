f240dea1050b24234dde5e3654e7d5aaad3872ed
jgauthi <jgauthi@users.noreply.github.com>
2019-01-16 13:27:51 +0100

    Header: Show user login status

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,7 +2,8 @@ import {USER_LOGIN_SUCCESS} from "../actions/constants";
 
 export default (state = {
     token: null,
-    userId: null
+    userId: null,
+    isAuthenticated: false
 }, action) => {
     switch (action.type) {
         case USER_LOGIN_SUCCESS:
@@ -10,7 +11,8 @@ export default (state = {
             return {
                 ...state,
                 token: action.token,
-                userId: action.userId
+                userId: action.userId,
+                isAuthenticated: true
             };
         default:
             return state;
