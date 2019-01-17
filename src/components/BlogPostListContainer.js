67ba3ddcef9b84bcc2bfdda55f0ff192d149192d
jgauthi <jgauthi@users.noreply.github.com>
2019-01-17 19:31:20 +0100

    BlogPost: Query param based pagination of blog posts

--- src/components/BlogPostListContainer.js
+++ src/components/BlogPostListContainer.js
@@ -15,24 +15,34 @@ const mapDispatchToProps = {
 
 class BlogPostListContainer extends React.Component {
     componentDidMount() {
-        this.props.blogPostListFetch();
+        this.props.blogPostListFetch( this.getQueryParamPage() );
     }
 
     componentDidUpdate(prevProps) {
-        const {currentPage, blogPostListFetch} = this.props;
+        const {currentPage, blogPostListFetch, blogPostListSetPage} = this.props;
 
-        if (prevProps.currentPage !== currentPage) {
-            blogPostListFetch(currentPage);
+        if (prevProps.currentPage !== this.getQueryParamPage()) {
+            blogPostListFetch( this.getQueryParamPage() );
         }
     }
 
+    getQueryParamPage() {
+        return Number(this.props.match.params.page) || 1;
+    }
+
+    changePage(page) {
+        const {history, blogPostListSetPage} = this.props;
+        blogPostListSetPage(page);
+        history.push(`/${page}`);
+    }
+
     render() {
-        const {posts, isFetching, blogPostListSetPage, currentPage} = this.props;
+        const {posts, isFetching, currentPage} = this.props;
 
         return (
             <div>
                 <BlogPostList posts={posts} isFetching={isFetching} />
-                <Paginator currentPage={currentPage} pageCount={10} setPage={blogPostListSetPage}/>
+                <Paginator currentPage={currentPage} pageCount={10} setPage={this.changePage.bind(this)}/>
             </div>
             )
     }
